7b787abe193adfde7981cffe40c8f297586c9cd6
Van Trung <trungvan2811@gmail.com>
2023-06-09 21:06:09 +0700

    first commit devmaster

---
+++ csdl.sql
@@ -0,0 +1,10 @@
+create database csdl
+use csdl 
+create table user (
+    id int,
+    name varchar(29),
+    prices float,
+    dates date
+
+)
+insert into user(id,name,prices,dates) values(1,'ha',80,'2020')
